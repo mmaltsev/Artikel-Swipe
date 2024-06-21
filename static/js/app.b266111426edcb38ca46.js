76dde6fcdebec203c296cad77f06143d73289851
mmaltsev <d.kruz@mail.ru>
2024-06-21 18:59:18 +0200

    ran build

--- static/js/app.b266111426edcb38ca46.js
+++ static/js/app.b266111426edcb38ca46.js
@@ -1,6 +1,6 @@
 webpackJsonp([0],{
 
-/***/ "IbhS":
+/***/ "5DPp":
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
@@ -240,7 +240,7 @@ const DAS_ARTICLE = 'cardSkipped';
     }
   }
 });
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-dd018e36","hasScoped":true,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/components/GameCard.vue
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-7cdda6e7","hasScoped":true,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/components/GameCard.vue
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return (_vm.isShowing)?_c('div',{ref:"interactElement",staticClass:"card",class:{
     isAnimating: _vm.isInteractAnimating,
     isCurrent: _vm.isCurrent
@@ -250,7 +250,7 @@ var esExports = { render: render, staticRenderFns: staticRenderFns }
 /* harmony default export */ var components_GameCard = (esExports);
 // CONCATENATED MODULE: ./src/components/GameCard.vue
 function injectStyle (ssrContext) {
-  __webpack_require__("IbhS")
+  __webpack_require__("5DPp")
 }
 var normalizeComponent = __webpack_require__("VU/8")
 /* script */
@@ -263,7 +263,7 @@ var __vue_template_functional__ = false
 /* styles */
 var __vue_styles__ = injectStyle
 /* scopeId */
-var __vue_scopeId__ = "data-v-dd018e36"
+var __vue_scopeId__ = "data-v-7cdda6e7"
 /* moduleIdentifier (server only) */
 var __vue_module_identifier__ = null
 var Component = normalizeComponent(
@@ -315,14 +315,14 @@ var Component = normalizeComponent(
     }
   }
 });
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-56a969ae","hasScoped":true,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/components/GameCardsStack.vue
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-bea52930","hasScoped":true,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/components/GameCardsStack.vue
 var GameCardsStack_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"cards"},[_vm._l((_vm.cards),function(card,index){return _c('GameCard',{key:(card.english_translation + index),attrs:{"cardWord":card.word,"cardArticle":card.article,"cardEnTranslation":card.english_translation,"is-current":index === 0},on:{"cardAccepted":function($event){return _vm.$emit('cardAccepted');},"cardRejected":function($event){return _vm.$emit('cardRejected');},"cardSkipped":function($event){return _vm.$emit('cardSkipped');},"hideCard":function($event){return _vm.$emit('hideCard');}}})}),_vm._v(" "),_vm._m(0)],2)}
 var GameCardsStack_staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"tags"},[_c('div',{staticClass:"tag tag-der noselect"},[_vm._v("Der "),_c('span',{staticClass:"tag-arrow"},[_vm._v("→")])]),_vm._v(" "),_c('div',{staticClass:"tag tag-die noselect"},[_c('span',{staticClass:"tag-arrow"},[_vm._v("←")]),_vm._v(" Die")]),_vm._v(" "),_c('div',{staticClass:"tag tag-das noselect"},[_vm._v("Das "),_c('span',{staticClass:"tag-arrow"},[_vm._v("↑")])])])}]
 var GameCardsStack_esExports = { render: GameCardsStack_render, staticRenderFns: GameCardsStack_staticRenderFns }
 /* harmony default export */ var components_GameCardsStack = (GameCardsStack_esExports);
 // CONCATENATED MODULE: ./src/components/GameCardsStack.vue
 function GameCardsStack_injectStyle (ssrContext) {
-  __webpack_require__("ePaT")
+  __webpack_require__("Of1Q")
 }
 var GameCardsStack_normalizeComponent = __webpack_require__("VU/8")
 /* script */
@@ -335,7 +335,7 @@ var GameCardsStack___vue_template_functional__ = false
 /* styles */
 var GameCardsStack___vue_styles__ = GameCardsStack_injectStyle
 /* scopeId */
-var GameCardsStack___vue_scopeId__ = "data-v-56a969ae"
+var GameCardsStack___vue_scopeId__ = "data-v-bea52930"
 /* moduleIdentifier (server only) */
 var GameCardsStack___vue_module_identifier__ = null
 var GameCardsStack_Component = GameCardsStack_normalizeComponent(
@@ -354,6 +354,9 @@ var german_nouns = __webpack_require__("OM3/");
 var german_nouns_default = /*#__PURE__*/__webpack_require__.n(german_nouns);
 
 // CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/lib/selector.js?type=script&index=0!./src/App.vue
+//
+//
+//
 //
 //
 //
@@ -422,14 +425,14 @@ const NUMBER_OF_NOUNS = 2000;
     }
   }
 });
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-0721ea00","hasScoped":false,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/App.vue
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/template-compiler?{"id":"data-v-5e60539d","hasScoped":false,"transformToRequire":{"video":["src","poster"],"source":"src","img":"src","image":"xlink:href"},"buble":{"transforms":{}}}!./node_modules/vue-loader/lib/selector.js?type=template&index=0!./src/App.vue
 var App_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{attrs:{"id":"app"}},[_c('GameCardsStack',{attrs:{"cards":_vm.visibleCards},on:{"cardAccepted":_vm.handleCardAccepted,"cardRejected":_vm.handleCardRejected,"cardSkipped":_vm.handleCardSkipped,"hideCard":_vm.removeCardFromDeck}}),_vm._v(" "),_vm._m(0)],1)}
-var App_staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"footer"},[_vm._v("\n      Inspired by\n      "),_c('a',{attrs:{"href":"https://apps.apple.com/de/app/derdasdie-german-articles/id6480586707"}},[_vm._v("DerDasDie. German articles")])])}]
+var App_staticRenderFns = [function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"footer"},[_vm._v("\n      Inspired by\n      "),_c('a',{attrs:{"href":"https://apps.apple.com/de/app/derdasdie-german-articles/id6480586707","target":"_blank"}},[_vm._v("DerDasDie. German articles")])])}]
 var App_esExports = { render: App_render, staticRenderFns: App_staticRenderFns }
 /* harmony default export */ var selectortype_template_index_0_src_App = (App_esExports);
 // CONCATENATED MODULE: ./src/App.vue
 function App_injectStyle (ssrContext) {
-  __webpack_require__("xALl")
+  __webpack_require__("T+5v")
 }
 var App_normalizeComponent = __webpack_require__("VU/8")
 /* script */
@@ -480,14 +483,14 @@ module.exports = [{"english_translation":"Time","article":"Die","word":"Zeit"},{
 
 /***/ }),
 
-/***/ "ePaT":
+/***/ "Of1Q":
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ }),
 
-/***/ "xALl":
+/***/ "T+5v":
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
@@ -495,4 +498,4 @@ module.exports = [{"english_translation":"Time","article":"Die","word":"Zeit"},{
 /***/ })
 
 },["NHnr"]);
-//# sourceMappingURL=app.cecfdee1019a636802a3.js.map
+//# sourceMappingURL=app.b266111426edcb38ca46.js.map
